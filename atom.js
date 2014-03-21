ba9fecbb7c86d19eb47958dc21ca741d12c42482
hnagato <nagato@team-lab.com>
2014-03-22 00:06:45 +0900

    Bug fix
    - wrong variable and function scopes
    - change event names overall

--- atom.js
+++ atom.js
@@ -1,38 +1,42 @@
-var textareas = {};
-
-function openInAtom(textarea) {
-  if (textareas[textarea]) {
+(function(textareas) {
+  if (window.self !== window.top) {
     return;
   }
 
-  safari.self.addEventListener('message', function(event) {
-    switch (event.name) {
-    case 'textchanged':
-      textarea.value = event.message;
-      break;
-
-    case 'disconnect':
-      textarea.removeEventListener('input', onInput);
-      delete textareas[textarea];
-      break;
+  function openInAtom(textarea) {
+    if (textareas[textarea]) {
+      return;
     }
-  }, false);
 
-  function onInput() {
-    safari.self.tab.dispatchMessage('textchanged', textarea.value);
+    safari.self.addEventListener('message', function(event) {
+      switch (event.name) {
+      case 'atom-textchanged':
+        textarea.value = event.message;
+        break;
+
+      case 'ws-disconnected':
+        textarea.removeEventListener('input', onInput);
+        delete textareas[textarea];
+        break;
+      }
+    }, false);
+
+    safari.self.tab.dispatchMessage('ws-connect', location.origin);
+
+    function onInput(e) {
+      safari.self.tab.dispatchMessage('page-textchanged', textarea.value);
+    }
+    onInput();
+    textarea.addEventListener('input', onInput, true);
+    textareas[textarea] = true;
   }
-  textarea.addEventListener('onchange', onInput);
 
-  safari.self.tab.dispatchMessage('connect', location.origin);
+  Array.prototype.slice.call(document.querySelectorAll('textarea')).forEach(function(t) {
+    t.addEventListener('keydown', function(e) {
+      if (e.keyCode == 69 && e.shiftKey && e.metaKey) {
+        openInAtom(t);
+      }
+    });
+  });
 
-  onInput();
-  textareas[textarea] = true;
-}
-
-document.addEventListener('keydown', function(event) {
-  if (event.target.nodeName === 'TEXTAREA' &&
-      event.keyCode == 69 &&
-      event.shiftKey && event.metaKey) {
-    openInAtom(event.target);
-  }
-});
+})({});
